8556028a5ca9a78111870cbf45c0d66026386bb7
Bryan Cauã <bryanncaua@gmail.com>
2026-05-06 21:46:22 -0300

    Adição de titulo e descrição aos vídeos vindo do .json

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const api = fetch("http://localhost:3000/videos")
         containerVideos.innerHTML += `
         <li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+        <div class="descricao-video">
+            <img src="${video.imagem}" alt="Logo do canal" class="img-canal">
+            <h3 class="titulo-video">${video.titulo}</h3>
+            <p class="titulo-canal">${video.descricao}</p>
+        </div>
         </li>
         `
     })
